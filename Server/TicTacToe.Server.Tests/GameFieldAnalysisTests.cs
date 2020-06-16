23c4437b7769e0b23d1ab1ac639d3c9d6f2a8466
LazeR <jiazer0k@gmail.com>
2020-06-16 23:29:55 +0300

    Misc: add winner definer

---
+++ Server/TicTacToe.Server.Tests/GameFieldAnalysisTests.cs
@@ -0,0 +1,87 @@
+﻿using NUnit.Framework;
+using TicTacToe.Shared;
+
+namespace TicTacToe.Server.Tests
+{
+    [TestFixture]
+    public sealed class GameFieldAnalysisTests
+    {
+        private GameFieldAnalysis m_analysis = default;
+        [SetUp]
+        public void Initialize()
+        {
+            m_analysis = new GameFieldAnalysis();
+
+        }
+
+        [Test]
+        public void NoWinOne()
+        {
+            CellModel[,] field = {
+                { null, null, null },
+                { null, null, null } ,
+                { null, null, null } };
+
+            bool win = m_analysis.WinnerDefiner(field, out _);
+            Assert.IsFalse(win);
+        }
+
+        [Test]
+        public void NoWinTwo()
+        {
+            CellModel[,] field = {
+                { new CellModel(Symbols.X), new CellModel(Symbols.X), new CellModel(Symbols.O) },
+                { null, new CellModel(Symbols.X), null } ,
+                { new CellModel(Symbols.O), null, new CellModel(Symbols.O) } };
+
+            bool win = m_analysis.WinnerDefiner(field, out _);
+            Assert.IsFalse(win);
+        }
+
+        [Test]
+        public void HorizontalTestOne()
+        {
+            CellModel[,] field = {
+                { new CellModel(Symbols.X), new CellModel(Symbols.X), new CellModel(Symbols.X) },
+                { new CellModel(Symbols.O), new CellModel(Symbols.O), null},
+                { new CellModel(Symbols.O), null, null} };
+            bool win = m_analysis.WinnerDefiner(field, out _);
+            Assert.IsTrue(win);
+        }
+
+        [Test]
+        public void HorizontalTestTwo()
+        {
+            CellModel[,] field = {
+                { null, null, null},
+                { new CellModel(Symbols.X), new CellModel(Symbols.X), new CellModel(Symbols.X) },
+                { null, null, null} };
+            bool win = m_analysis.WinnerDefiner(field, out _);
+            Assert.IsTrue(win);
+        }
+
+        [Test]
+        public void HorizontalTestThree()
+        {
+            CellModel[,] field = {
+                { null, null, null},
+                { null, null, null} ,
+                { new CellModel(Symbols.X), new CellModel(Symbols.X), new CellModel(Symbols.X) }
+               };
+            bool win = m_analysis.WinnerDefiner(field, out _);
+            Assert.IsTrue(win);
+        }
+
+        [Test]
+        public void DiagonalTestOne()
+        {
+            CellModel[,] field = {
+                { new CellModel(Symbols.X), null, null},
+                { null, new CellModel(Symbols.X), null} ,
+                { new CellModel(Symbols.O), new CellModel(Symbols.X), new CellModel(Symbols.X) }
+            };
+            bool win = m_analysis.WinnerDefiner(field, out _);
+            Assert.IsTrue(win);
+        }
+    }
+}
--- Server/TicTacToe.Server/State/GameField/ServerFieldModel.cs
+++ Server/TicTacToe.Server/State/GameField/ServerFieldModel.cs
@@ -1,22 +1,14 @@
 using System;
-using Microsoft.Extensions.Logging;
+using System.Numerics;
 using TicTacToe.Shared;
 
 namespace TicTacToe.Server
 {
-    public sealed class ServerFieldModel :
-        ICheckableField
-        , ISettableField
+    public sealed class ServerFieldModel
     {
-        private readonly ILogger<ServerFieldModel> m_logger = default;
         private readonly CellModel[,] m_field = new CellModel[FieldSize.Value, FieldSize.Value];
 
-        public ServerFieldModel(ILogger<ServerFieldModel> logger)
-        {
-            m_logger = logger;
-        }
-
-        bool ICheckableField.IsEmpty(int x, int y)
+        public bool IsEmpty(int x, int y)
         {
             if (!IsValid(x, y))
             {
@@ -26,20 +18,78 @@ namespace TicTacToe.Server
             return m_field[x, y] == null;
         }
 
-        void ISettableField.Set(Symbols symbol, int x, int y)
-        {
-            if (!IsValid(x, y))
-            {
-                throw new Exception("Invalid field coordinates!");
-            }
-
-            m_field[x, y] = new CellModel(symbol);
-            m_logger.LogInformation($"Filed was changed ({x},{y}) to {symbol}");
-        }
-
         private bool IsValid(int x, int y)
         {
             return x >= 0 && x < FieldSize.Value && y >= 0 && y < FieldSize.Value;
+        }
+    }
+
+
+    public sealed class GameFieldAnalysis
+    {
+        private struct Direction
+        {
+            public readonly int X;
+            public readonly int Y;
+
+            public Direction(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+        }
+
+        private readonly Direction[] m_winDirections = new[]
+        {
+            new Direction(1, 0), // horizontal
+            new Direction(0, 1), // vertical
+            new Direction(1, 1), //  diagonal
+            new Direction(-1, -1) //  diagonal - reverting
+        };
+
+        private const uint WinLength = 3;
+
+        public bool WinnerDefiner(CellModel[,] field, out Symbols? symbol)
+        {
+            symbol = null;
+            for (int y = 0; y < FieldSize.Value; y++)
+            {
+                for (int x = 0; x < FieldSize.Value; x++)
+                {
+                    foreach (Direction direction in m_winDirections)
+                    {
+                        if (x + (direction.X * WinLength) <= FieldSize.Value &&
+                            x + (direction.X * WinLength) >= 0 &&
+                            y + (direction.Y * WinLength) <= FieldSize.Value &&
+                            y + (direction.Y * WinLength) >= 0)
+                        {
+                            CellModel lastCell = field[y, x];
+                            if (lastCell != null)
+                            {
+                                for (int k = 1; k < WinLength; k++)
+                                {
+                                    CellModel cell = field[y + (direction.Y * k), x + (direction.X * k)];
+                                    if (cell == null || cell.Symbol != lastCell.Symbol)
+                                    {
+                                        lastCell = null;
+                                        break;
+                                    }
+                                    else
+                                    {
+                                        lastCell = cell;
+                                    }
+                                }
+                            }
+                            if (lastCell != null)
+                            {
+                                symbol = lastCell.Symbol;
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+            return false;
         }
     }
 }
