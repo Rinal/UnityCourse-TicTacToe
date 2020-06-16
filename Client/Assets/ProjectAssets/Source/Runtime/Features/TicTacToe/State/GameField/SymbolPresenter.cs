5013547bbfbebc0b57bffe2c48252d32eb24eee2
Stanislav Felinskyi <felinskyi.sg@gmail.com>
2020-06-16 22:58:26 +0300

    Misc: Symbol presenter

--- Client/Assets/ProjectAssets/Source/Runtime/Features/TicTacToe/State/GameField/SymbolPresenter.cs
+++ Client/Assets/ProjectAssets/Source/Runtime/Features/TicTacToe/State/GameField/SymbolPresenter.cs
@@ -1,31 +1,9 @@
 using Innovecs.DialoguesSystem;
 using TicTacToe.Shared;
-using TMPro;
-using UnityEngine;
-using UnityEngine.Assertions;
 
 namespace TicTacToe.Client
 {
-    public sealed class SymbolPresenter : BaseValuePresenter<Symbols>
+    public abstract class SymbolPresenter : BaseValuePresenter<Symbols>
     {
-        [SerializeField] private TextMeshProUGUI m_symbolText = default;
-
-        private void Awake()
-        {
-            Assert.IsNotNull(m_symbolText);
-        }
-
-        public override void Show(Symbols target)
-        {
-            base.Show(target);
-            m_symbolText.text = Target.ToString();
-            m_symbolText.gameObject.SetActive(true);
-        }
-
-        public override void Hide()
-        {
-            m_symbolText.gameObject.SetActive(false);
-            base.Hide();
-        }
     }
 }
---
+++ Client/Assets/ProjectAssets/Source/Runtime/Features/TicTacToe/State/GameField/SymbolPresenterUgui.cs
@@ -0,0 +1,30 @@
+using TicTacToe.Shared;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace TicTacToe.Client
+{
+    public sealed class SymbolPresenterUgui : SymbolPresenter
+    {
+        [SerializeField] private TextMeshProUGUI m_symbolText = default;
+
+        private void Awake()
+        {
+            Assert.IsNotNull(m_symbolText);
+        }
+
+        public override void Show(Symbols target)
+        {
+            base.Show(target);
+            m_symbolText.text = Target.ToString();
+            m_symbolText.gameObject.SetActive(true);
+        }
+
+        public override void Hide()
+        {
+            m_symbolText.gameObject.SetActive(false);
+            base.Hide();
+        }
+    }
+}
